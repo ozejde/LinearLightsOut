a172920ff74b94c8cbc328174ea2a4b2bc6121c7
ejdeoz <ejdeoz@c675d368-4daa-467d-8ded-79259d5d57ce>
2016-10-11 20:02:02 +0000

    Random button symbols

--- LinearLightsOut/src/linearLightsOut/LinearMain.java
+++ LinearLightsOut/src/linearLightsOut/LinearMain.java
@@ -1,12 +1,13 @@
 package linearLightsOut;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 /**
  * Runs the Linear Lights Out application.
  * 
- * @author <TODO: YOUR NAME HERE>. Created Jan 18, 2010.
+ * @author Olivia Ejde. Created Oct 10, 2016.
  */
 public class LinearMain {
 
@@ -16,12 +17,20 @@ public class LinearMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String nButtonsString = JOptionPane
-				.showInputDialog("How many buttons would you like?");
+		String nButtonsString = JOptionPane.showInputDialog("How many buttons would you like?");
 		int nButtons = Integer.parseInt(nButtonsString);
-		JFrame myFrame = new JFrame();
-		// you'll want to think about how you want to manage the state of the game
-		//also you want to add some buttons and stuff
+		
+		JFrame myFrame = new JFrame("Linear Lights Out!");
+		BoxLayout boxLayout = new BoxLayout(myFrame.getContentPane(), BoxLayout.X_AXIS);
+		myFrame.setLayout(boxLayout);
+		
+		for (int i = 0; i < nButtons; i++) {
+			
+			myFrame.add(new LightButton());
+		}
+		// you'll want to think about how you want to manage the state of the
+		// game
+		// also you want to add some buttons and stuff
 		myFrame.setVisible(true);
 	}
 }
