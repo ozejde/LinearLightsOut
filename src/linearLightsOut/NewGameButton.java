896e02f396f8407e4ad1540d5c9368ce267c89d1
Olivia Ejde <ozejde@users.noreply.github.com>
2016-10-12 19:56:39 +0000

    Fully commented code

--- src/linearLightsOut/NewGameButton.java
+++ src/linearLightsOut/NewGameButton.java
@@ -4,18 +4,23 @@ import java.awt.Dimension;
 
 import javax.swing.JButton;
 
-public class NewGameButton extends JButton{
-	private LightsOutFrame frame;
-
+/**
+ * 
+ * A JButton that restarts the game when pressed
+ *
+ * @author ejdeoz. Created Oct 10, 2016.
+ */
+public class NewGameButton extends JButton {
+	/**
+	 * 
+	 * Creates a new NewGameButton
+	 *
+	 */
 	public NewGameButton() {
 		super();
 		this.setText("New Game");
-		this.setPreferredSize(new Dimension(100,30));
+		this.setPreferredSize(new Dimension(100, 30));
 		this.addActionListener(new ButtonListener(this, "NewGameButton"));
 	}
-	
-	public void getFrame(LightsOutFrame frame){
-		this.frame = frame;
-	}
-	
+
 }
--- src/linearLightsOut/QuitButton.java
+++ src/linearLightsOut/QuitButton.java
@@ -4,17 +4,26 @@ import java.awt.Dimension;
 
 import javax.swing.JButton;
 
-public class QuitButton extends JButton{
+/**
+ * 
+ * A JButton that quits the game when pressed
+ *
+ * @author ejdeoz. Created Oct 10, 2016.
+ */
+public class QuitButton extends JButton {
 	private LightsOutFrame frame;
 
+	/**
+	 * 
+	 * Creates a new QuitButton
+	 *
+	 */
 	public QuitButton() {
 		super();
 		this.setText("Quit");
-		this.setPreferredSize(new Dimension(70,30));
-		
+		this.setPreferredSize(new Dimension(70, 30));
+
 		this.addActionListener(new ButtonListener(this, "QuitButton"));
 	}
-	
 
 }
-
